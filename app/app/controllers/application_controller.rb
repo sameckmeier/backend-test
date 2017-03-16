c006cb907de3042ae22f7b2b9d340b8c5f84cc96
Sam Eckmeier <eckmeier41@gmail.com>
2017-03-16 00:56:24 -0400

    adds authenticate_request to validate JWT

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  attr_reader :current_user
+
+  def authenticate_request
+    begin
+      user_id = token_payload[:user_id]
+
+      if user_id
+        @current_user = User.find(user_id)
+      else
+        render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+      end
+    rescue JWT::VerificationError, JWT::DecodeError
+      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def token_payload
+    token = params[:headers][:Authentication].split(' ')
+    @token_payload ||= JWTProcessor.decode(token[1])
+  end
 end
