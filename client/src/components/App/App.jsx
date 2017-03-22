3fe7e7f2a37e5670d4c91a66a28a68b09704f79a
Sam Eckmeier <eckmeier41@gmail.com>
2017-03-22 10:32:25 -0400

    updates App to include different page view types and login/logged out conditional components

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,50 +2,92 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setView } from '../../redux/ducks/view';
 import styles from './App.scss';
-import Events from '../Events';
+import Meetings from '../Meetings';
 import Event from '../Event';
+import CreateEvent from '../CreateEvent';
+import Login from '../Login';
 import back from '../../assets/images/back.svg';
+import { getUser } from '../../redux/ducks/user';
+import { logOut } from '../../redux/ducks/authentication';
+import { defaultCreateEventState } from '../../redux/ducks/createEventParams';
+
 import {
-  EVENTS,
+  MEETINGS,
   EVENT,
   CREATE_EVENT,
+  LOGIN,
 } from '../../redux/ducks/view';
 
 const App = props => {
-  const { view, setView } = props;
+  const {
+    authentication,
+    view,
+    setView,
+    getUser,
+    logOut,
+    defaultCreateEventState,
+  } = props;
+
+  const {
+    loggedIn
+  } = authentication;
+
+  const handleBackClick = () => {
+    if (view === CREATE_EVENT) { defaultCreateEventState() }
+
+    setView(MEETINGS);
+  };
+
+  if (!loggedIn && localStorage.getItem('jwt')) { getUser() }
 
   let renderView;
 
   switch(view) {
-    case EVENTS:
-      renderView = <Events />;
+    case MEETINGS:
+      renderView = <Meetings />;
       break;
     case EVENT:
       renderView = <Event />;
       break;
     case CREATE_EVENT:
+      renderView = <CreateEvent />;
+      break;
+    case LOGIN:
+      renderView = <Login />;
       break;
     default:
       throw new Error('Invalid view type');
   }
 
-  return <div className={styles.container}>
-    <header className={styles.header}>
-      {view !== EVENTS &&
-        <button className={ styles.backButton }>
-          <img src={back} alt="Back" onClick={ () => setView(EVENTS) }/>
+  return <div className={ styles.container }>
+    <header className={ styles.header }>
+      <div className={ styles.headerLeft }>
+        {view !== MEETINGS &&
+          <button className={ styles.backButton } onClick={ handleBackClick }>
+            <img src={ back } alt="Back" />
+          </button>
+        }
+        <h1>Events</h1>
+      </div>
+      <div className={ styles.headerRight }>
+        <button className={ styles.login } onClick={ () => loggedIn ? logOut() : setView(LOGIN) }>
+          { loggedIn ? "Log Out" : "Login" }
         </button>
-      }
-      <h1>Events</h1>
+      </div>
     </header>
-    <main className={styles.main}>
+    <main className={ styles.main }>
       { renderView }
     </main>
-    <footer className={styles.footer} />
+    <footer className={ styles.footer } />
   </div>;
 };
 
 export default connect(
-  state => ({ view: state.view }),
-  dispatch => ({ setView: view => dispatch(setView(view)) }),
+  state => ({ view: state.view, authentication: state.authentication }),
+  dispatch => ({
+    setView: view => dispatch(setView(view)),
+    getUser: () => dispatch(getUser()),
+    logOut: () => dispatch(logOut()),
+    defaultCreateEventState: () => dispatch(defaultCreateEventState()),
+  }),
 )(App);
