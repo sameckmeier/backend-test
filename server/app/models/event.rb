0d915473160dde6eeb7df397ef7861b7260c63d9
Sam Eckmeier <eckmeier41@gmail.com>
2017-03-22 10:40:57 -0400

    adds ! to create calls and method to turn event and relationships into json

--- server/app/models/event.rb
+++ server/app/models/event.rb
@@ -5,13 +5,20 @@ class Event < ActiveRecord::Base
   def self.build(params)
     params = params.clone()
     meetings = params.delete(:meetings)
-    event = create(params)
+    event = create!(params)
 
     meetings.each do |m|
       m[:event_id] = event.id
-      Meeting.build(m)
+      Meeting.create!(m)
     end
 
     event
+  end
+
+  def self.jsonify(event)
+    event_json = event.as_json
+    event_json[:meetings] = event.meetings.map { |m| m.as_json }
+    
+    event_json
   end
 end
