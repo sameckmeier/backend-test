778f1c3c92cf55a15f0ddc1dc92abacd45684d0c
Sam Eckmeier <eckmeier41@gmail.com>
2017-03-22 10:43:05 -0400

    adds timezone and utc_offset fields and changes datetime to date_time

--- server/db/migrate/20170315211746_create_meetings.rb
+++ server/db/migrate/20170315211746_create_meetings.rb
@@ -1,9 +1,11 @@
 class CreateMeetings < ActiveRecord::Migration
   def change
     create_table :meetings do |t|
-      t.timestamp :datetime, null: false
+      t.timestamp :date_time, null: false
+      t.decimal :utc_offset, precision: 4, null: false
       t.decimal :price, precision: 8, scale: 2, default: 0
       t.string :location, null: false
+      t.string :timezone, null: false
       t.belongs_to :event
 
       t.timestamps null: false
