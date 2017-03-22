dd7a600614c54a4d3381fcd7560e57b3bb1d8616
Sam Eckmeier <eckmeier41@gmail.com>
2017-03-22 10:40:06 -0400

    wraps actions in error handlers, adds strong parameters for event creation, and authenticates create actions

--- server/app/controllers/events_controller.rb
+++ server/app/controllers/events_controller.rb
@@ -1,18 +1,40 @@
 class EventsController < ApplicationController
-  def index
-    events = Event.includes(:meetings).map do |e|
-      event_json = e.as_json
-      event_json[:meetings] = e.meetings.map do |m|
-        meeting_json = m.as_json
-        meeting_json[:time] = m.time
-        meeting_json[:date] = m.date
+  before_action :authenticate_request, only: [:create]
 
-        meeting_json
-      end
+  def show
+    render({ json: { event: Event.jsonify(Event.find(params[:id])) }, status: 200 })
+  end
 
-      event_json
+  def create
+    begin
+      params[:user_id] = @current_user.id
+
+      event = Event.build(event_params)
+      event = Event.jsonify(event)
+
+      render({ json: { event: event }, status: 201 })
+    rescue StandardError => e
+      puts e.message
+      puts e.backtrace
+
+      render({ json: { errors: ['Unable to create event'] }, status: 500 })
     end
-    
-    render({ json: { events: events }})
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit([
+      :title,
+      :description,
+      :image_url,
+      meetings: [
+        :price,
+        :location,
+        :date_time,
+        :utc_offset,
+        :timezone
+      ],
+    ])
   end
 end
