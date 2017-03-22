90d7e6655f9c247a7119ae535e993d24e0d52800
Sam Eckmeier <eckmeier41@gmail.com>
2017-03-22 10:35:19 -0400

    includes css loader

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -139,10 +139,10 @@ module.exports = {
       // "style" loader turns CSS into JS modules that inject <style> tags.
       // In production, we use a plugin to extract that CSS to a file, but
       // in development "style" loader enables hot editing of CSS.
-      // {
-      //   test: /\.css$/,
-      //   loader: 'style!css?importLoaders=1!postcss'
-      // },
+      {
+        test: /\.css$/,
+        loader: 'style!css?importLoaders=1!postcss'
+      },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
       {
